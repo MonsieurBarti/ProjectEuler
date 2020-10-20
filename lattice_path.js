e2107d0cc7b2c24b2fcf496ac2ef165f97307738
Monsieur Barti <pierre.le-corff@epita.fr>
2020-10-20 17:29:34 +0200

    ajout lattice_path

--- lattice_path.js
+++ lattice_path.js
@@ -1,10 +1,10 @@
 // Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
 // How many such routes are there through a 20×20 grid?
-let grid = [
-    [0, 0],
-    [0, 0]
-]
-let result = grid => {
-
+let result = n => {
+    for (var i = 1, c = 1; i <= n; i++) {
+        console.log(c,"* (", n, "+", i, ") /", i)
+        c = c * (n + i) / i
+    }
+    return c
 }
-console.log(result())
+console.log(result(20))
